b5e61fd38f53b11cd74daa4169dd2c21612de590
zhangxinmei <2536075245@qq.com>
2018-05-06 22:33:40 +0800

    add code

--- shell/src/components/header/header.vue
+++ shell/src/components/header/header.vue
@@ -15,23 +15,63 @@
           <span class="text">{{seller.supports[0].description}}</span>
         </div>
       </div>
-      <div v-if="seller.supports" class="support-count">
+      <div v-if="seller.supports" class="support-count" @click="showDetail">
         <span class="count">{{seller.supports.length}}个</span>
         <i class="icon-keyboard_arrow_right"></i>
       </div>
     </div>
-    <div class="bulletin-wrap">
+    <div class="bulletin-wrap" @click="showDetail">
       <span class="bulletin-title"></span>
       <span class="bulletin-text">{{seller.bulletin}}</span>
     </div>
     <div class="herder-bg">
       <img :src="seller.avatar" alt="">
     </div>
+    <div v-show="isDetailShow" class="detial">
+      <!--CSS sticky footer -->
+      <div class="detail-wrap clearfix">
+        <div class="detail-mian">
+          <div class="detail-hd">
+            <h1>{{seller.name}}</h1>
+            <div class="hd-star">
+              <star :size="48" :score="seller.score"></star>
+            </div>
+          </div>
+          <headline :titleText="titleTextOffer"></headline>
+          <ul class="supports" v-if="seller.supports">
+            <li class="supports-item" v-for="(item,index) in seller.supports" :key="index">
+              <span class="icon" :class="classMsp[seller.supports[index].type]"></span>
+              <span class="text">{{seller.supports[index].description}}</span>
+            </li>
+          </ul>
+          <headline :titleText="titleTextBulletin"></headline>
+          <div class="bulletin">
+            <p class="content">{{seller.bulletin}}</p>
+          </div>
+        </div>
+      </div>
+      <div class="detail-cloase" @click="isDetailShow=false">
+        <i class="icon-close"></i>
+      </div>
+    </div>
   </div>
 </template>
 
 <script type='text/ecmascript-6'>
+import star from "../star/star";
+import headline from '../title/title'
 export default {
+  data() {
+    return {
+      isDetailShow: false,
+      titleTextOffer:'优惠活动',
+      titleTextBulletin:'商家公告'
+    };
+  },
+  components: {
+    star,
+    headline
+  },
   props: {
     seller: {
       type: Object
@@ -39,6 +79,11 @@ export default {
   },
   created() {
     this.classMsp = ["decrease", "discount", "special", "invoice", "guarantee"];
+  },
+  methods: {
+    showDetail() {
+      this.isDetailShow = true;
+    }
   }
 };
 </script>
@@ -46,8 +91,9 @@ export default {
 <style lang="less" rel='stylesheet/less'>
 .header {
   position: relative;
+  overflow: hidden;
   color: #fff;
-   background-color: rgba(7, 17, 27, 0.2);
+  background-color: rgba(7, 17, 27, 0.2);
   .content-wrap {
     position: relative;
     padding: 24px 12px 18px 24px;
@@ -163,7 +209,7 @@ export default {
       font-size: 10px;
     }
   }
-  .herder-bg{
+  .herder-bg {
     position: absolute;
     top: 0;
     left: 0;
@@ -171,10 +217,100 @@ export default {
     height: 100%;
     z-index: -1;
     filter: blur(10px);
-    img{
+    img {
       width: 100%;
       height: 100%;
-    
+    }
+  }
+  .detial {
+    position: fixed;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    z-index: 2;
+    background: rgba(7, 17, 27, 0.8);
+    .detail-wrap {
+      width: 100%;
+      min-height: 100%;
+      .detail-mian {
+        margin-top: 64px;
+        padding-bottom: 64px;
+        .detail-hd {
+          line-height: 16px;
+          font-size: 16px;
+          font-weight: 700;
+          text-align: center;
+          .hd-star {
+            margin-top: 18px;
+            padding: 2px 0;
+          }
+        }
+        .supports {
+          width: 80%;
+          margin: 0 auto;
+          .supports-item {
+            padding: 0 12px;
+            margin-bottom: 12px;
+            font-size: 0;
+            &:last-child {
+              margin-bottom: 0;
+            }
+            &::after{
+              display: inline-block;
+              vertical-align: middle;
+              content: '';
+              height: 100%;
+            }
+            .icon {
+              display: inline-block;
+              vertical-align: middle;
+              width: 16px;
+              height: 16px;
+              margin-right: 6px;
+              background-repeat: no-repeat;
+              background-size: 16px 16px;
+              &.decrease {
+                background-image: url(./img/decrease_2@2x.png);
+              }
+              &.discount {
+                background-image: url(./img/discount_2@2x.png);
+              }
+              &.guarantee {
+                background-image: url(./img/guarantee_2@2x.png);
+              }
+              &.invoice {
+                background-image: url(./img/invoice_2@2x.png);
+              }
+              &.special {
+                background-image: url(./img/special_2@3x.png);
+              }
+            }
+            .text{
+              display: inline-block;
+              vertical-align: middle;
+              font-size: 12px;
+            }
+          }
+        }
+        .bulletin{
+          width: 80%;
+          margin: 0 auto;
+          .content{
+            padding: 0 12px;
+            line-height: 24px;
+            font-size: 12px;
+          }
+        }
+      }
+    }
+    .detail-cloase {
+      position: relative;
+      width: 32px;
+      height: 32px;
+      margin: -64px auto 0 auto;
+      clear: both;
+      font-size: 32px;
     }
   }
 }
---
+++ shell/src/components/title/title.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="title">
+    <div class="line"></div>
+    <div class="text">{{titleText}}</div>
+    <div class="line"></div>
+  </div>
+</template>
+
+<script>
+export default {
+    props:['titleText']
+};
+</script>
+
+<style lang="less">
+.title {
+  display: flex;
+  width: 80%;
+  margin: 28px auto 24px auto;
+  .line {
+    position: relative;
+    flex: 1;
+    top: -6px;
+    border-bottom: 1px solid rgba(255, 255, 255, 0.2);
+  }
+  .text {
+    padding: 0 12px;
+    font-weight: 700;
+    font-size: 14px;
+  }
+}
+</style>
+
+
